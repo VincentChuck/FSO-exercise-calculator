188b4ed68c3f3a2e3f115845f84723a4704feae1
Vincent Chuck <vincent.chuck7@gmail.com>
2023-02-05 15:25:06 +0800

    Commit exercise 9.5

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -38,3 +38,5 @@ try {
   }
   console.log(errorMessage);
 }
+
+export default calculateBmi;
--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import bmiCalculator from './bmiCalculator';
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  res.send(bmiCalculator(height, weight));
 });
 
 const PORT = 3003;
